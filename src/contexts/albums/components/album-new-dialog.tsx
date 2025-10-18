568a2b63d2866d8f5ba544bbc3ca2fc7a2bf4f96
gabs <gabrielazbarreto@gmail.com>
2025-10-18 15:19:27 -0300

    refactor photo list components to use api

--- src/contexts/albums/components/album-new-dialog.tsx
+++ src/contexts/albums/components/album-new-dialog.tsx
@@ -10,41 +10,17 @@ import {
 } from "../../../components/dialog"
 import InputText from "../../../components/input-text"
 import Text from "../../../components/text"
-import type { Photo } from "../../photos/models/photo"
 import SelectCheckboxIllustration from "../../../assets/images/select-checkbox.svg?react"
 import Skeleton from "../../../components/skeleton"
 import PhotoImageSelectable from "../../photos/components/photo-image-selectable"
+import usePhotos from "../../photos/hooks/use-photos"
 
 interface AlbumNewDialogProps {
   trigger: React.ReactNode
 }
 
 export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
-  //TODO: mock de dados
-  const photos: Photo[] = [
-    {
-      id: "aaaaaa",
-      title: "hello world",
-      imageId: "portrait-tower.png",
-      albums: [
-        { id: "123", title: "hello" },
-        { id: "456", title: "world" },
-        { id: "789", title: "cat" },
-      ],
-    },
-
-    {
-      id: "12345",
-      title: "foto test",
-      imageId: "portrait-tower.png",
-      albums: [
-        { id: "123", title: "cat" },
-        { id: "456", title: "tower" },
-        { id: "789", title: "azul" },
-      ],
-    },
-  ]
-  const isLoadingPhotos = false
+  const { photos, isLoadingPhotos } = usePhotos()
 
   function handelTogglePhoto(selected: boolean, photoId: string) {
     console.log("selected photo: ", photoId, selected)
@@ -61,7 +37,7 @@ export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
 
           <div className="space-y-3">
             <Text as="div" variant="label-small" className="mb-3">
-              Fotos cadastradas
+              Adicione fotos ao novo álbum
             </Text>
 
             {!isLoadingPhotos && photos.length > 0 && (
@@ -69,7 +45,7 @@ export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
                 {photos.map((photo) => (
                   <PhotoImageSelectable
                     key={photo.id}
-                    src={`/images/${photo.imageId}`}
+                    src={`${import.meta.env.VITE_IMAGES_URL}/${photo.imageId}`}
                     title={photo.title}
                     imageClassName="w-20 h-20"
                     onSelectImage={(selected) =>
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -2,9 +2,11 @@ import Container from "../components/container"
 import AlbumsFilter from "../contexts/albums/components/albums-filter"
 import useAlbums from "../contexts/albums/hooks/use-albums"
 import PhotosList from "../contexts/photos/components/photos-list"
+import usePhotos from "../contexts/photos/hooks/use-photos"
 
 export default function PageHome() {
   const { albums, isLoadingAlbums } = useAlbums()
+  const { photos, isLoadingPhotos } = usePhotos()
 
   return (
     <Container>
@@ -13,31 +15,7 @@ export default function PageHome() {
         loading={isLoadingAlbums}
         className="mb-9"
       />
-      <PhotosList
-        photos={[
-          {
-            id: "aaaaaa",
-            title: "hello world",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "123", title: "hello" },
-              { id: "456", title: "world" },
-              { id: "789", title: "cat" },
-            ],
-          },
-
-          {
-            id: "12345",
-            title: "foto test",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "123", title: "cat" },
-              { id: "456", title: "tower" },
-              { id: "789", title: "azul" },
-            ],
-          },
-        ]}
-      />
+      <PhotosList photos={photos} loading={isLoadingPhotos} />
     </Container>
   )
 }
